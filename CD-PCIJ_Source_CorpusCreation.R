c06658f8c1f3f766d1c7d69d6a1bfaa9aabcd996
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-09-05 23:23:04 +0200

    fix tex paths

--- CD-PCIJ_Source_CorpusCreation.R
+++ CD-PCIJ_Source_CorpusCreation.R
@@ -12,8 +12,8 @@
 #'    number_sections: true
 #'    pandoc_args: --listings
 #'    includes:
-#'      in_header: temp/CD-PCIJ_Source_TEX_Preamble_EN.tex
-#'      before_body: [tex/CD-PCIJ_Source_TEX_Author.tex,tex/CD-PCIJ_Source_TEX_Definitions.tex,tex/CD-PCIJ_Source_TEX_CompilationTitle.tex]
+#'      in_header: tex/CD-PCIJ_Source_TEX_Preamble_EN.tex
+#'      before_body: [tex/CD-PCIJ_Source_TEX_Author.tex,temp/CD-PCIJ_Source_TEX_Definitions.tex,tex/CD-PCIJ_Source_TEX_CompilationTitle.tex]
 #'bibliography: temp/packages.bib
 #'nocite: '@*' 
 #'---
